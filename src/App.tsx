01c31e293dc5899d205d9e2d8f85108907b27b62
mrjeanp <47196328+mrjeanp@users.noreply.github.com>
2025-11-11 18:36:07 -0500

    added drag to deck and grave

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useCallback, useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
 import './App.css'
-import CardHelp from './lib/CardTemplate.tsx'
-import { Cell } from './lib/Cell.tsx'
-import { CircleNumber } from './lib/CircleNumber.tsx'
-import { BOARD, CARD_HEIGHT, CARD_WIDTH, CELL_HEIGHT, CELL_WIDTH, HAND, WIDTH } from './lib/constants.ts'
-import { Hand } from './lib/Hand.tsx'
-import type { CardData } from './lib/types.ts'
 import { useGameState } from './context/GameState.tsx'
 import Board from './lib/Board.tsx'
-import { createPortal } from 'react-dom'
-
+import CardHelp from './lib/CardTemplate.tsx'
+import { CircleNumber } from './lib/CircleNumber.tsx'
+import { CARD_HEIGHT, CARD_WIDTH, CELL_HEIGHT, CELL_WIDTH, WIDTH } from './lib/constants.ts'
+import { Hand } from './lib/Hand.tsx'
 
 function Game() {
-
-  const { state, dispatch } = useGameState()
-
-  const { deck, grave } = state
-
 
   const [row1Power, setRow1Power] = useState([0, 0])
 
   const [row2Power, setRow2Power] = useState([0, 0])
 
-
-
   return (
     <>
-      {createPortal(
-        <CardHelp />
-        , document.body)}
+      {createPortal(<CardHelp />, document.body)}
 
       <Board />
 
@@ -40,38 +28,111 @@ function Game() {
       <CircleNumber value={0} x={CELL_WIDTH / 2 - 21} y={CELL_HEIGHT / 2 * 3 - 42 - 10} color='red' />
       <CircleNumber value={row2Power[1]} x={CELL_WIDTH / 2 - 21} y={CELL_HEIGHT / 2 * 3 + 10} color='blue' />
 
-      <button style={{
-        position: 'absolute'
-        , top: CELL_HEIGHT * 2 + 2
-        , left: WIDTH - CARD_WIDTH - 10
-        , background: '#4f6072'
-        , color: 'white'
-        , fontWeight: 900
-        , width: CARD_WIDTH
-        , height: CARD_HEIGHT / 2
-      }}>Grave ({grave.length})</button>
+      <GraveButton />
 
-      <button
-        onClick={() => {
-          dispatch({
-            type: 'draw',
-          })
-        }}
-        style={{
-          position: 'absolute'
-          , top: CELL_HEIGHT * 3 - CARD_HEIGHT / 2 + 2
-          , left: WIDTH - CARD_WIDTH - 10
-          , background: '#d3eb0c'
-          , fontWeight: 900
-          , width: CARD_WIDTH
-          , height: CARD_HEIGHT / 2
-        }}>Deck ({deck.length})</button>
-
-
-
+      <DeckButton />
     </>
   )
 }
 
 
 export default Game
+
+
+
+function DeckButton() {
+  const { state, dispatch } = useGameState()
+  const { deck, dragging } = state
+
+  const ref = useRef<HTMLButtonElement>(null)
+
+  function handleDrop(e: React.DragEvent<HTMLButtonElement>): void {
+    e.preventDefault()
+
+    dispatch({
+      type: "addCard",
+      data: {
+        location: 'deck',
+        card: dragging?.card
+      }
+    })
+    dispatch({
+      type: "removeCard",
+      data: dragging
+    })
+  }
+
+  function handleDragOver(e: React.DragEvent<HTMLButtonElement>) {
+
+    e.preventDefault()
+
+    // if (!ref.current) return;
+
+  }
+
+  return <button
+    ref={ref}
+    onDrop={handleDrop}
+    onDragOver={handleDragOver}
+    onClick={() => {
+      dispatch({
+        type: 'draw',
+      })
+    }}
+    style={{
+      position: 'absolute',
+      top: CELL_HEIGHT * 3 - CARD_HEIGHT / 2 + 2,
+      left: WIDTH - CARD_WIDTH - 10,
+      background: '#d3eb0c',
+      fontWeight: 900,
+      width: CARD_WIDTH,
+      height: CARD_HEIGHT / 2
+    }}>Deck ({deck.length})</button>
+}
+
+
+function GraveButton() {
+  const { state, dispatch } = useGameState()
+  const { grave, dragging } = state
+
+  const ref = useRef<HTMLButtonElement>(null)
+
+  function handleDrop(e: React.DragEvent<HTMLButtonElement>): void {
+    e.preventDefault()
+
+    dispatch({
+      type: "addCard",
+      data: {
+        location: 'grave',
+        card: dragging?.card
+      }
+    })
+
+    dispatch({
+      type: "removeCard",
+      data: dragging
+    })
+  }
+
+  function handleDragOver(e: React.DragEvent<HTMLButtonElement>) {
+
+    e.preventDefault()
+
+    // if (!ref.current) return;
+
+  }
+  return <button
+    ref={ref}
+    onDrop={handleDrop}
+    onDragOver={handleDragOver}
+    style={{
+      position: 'absolute',
+      top: CELL_HEIGHT * 2 + 2,
+      left: WIDTH - CARD_WIDTH - 10,
+      background: '#4f6072',
+      color: 'white',
+      fontWeight: 900,
+      width: CARD_WIDTH,
+      height: CARD_HEIGHT / 2
+    }}>Grave ({grave.length})</button>
+}
